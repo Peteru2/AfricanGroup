c6ac5ddd094edb13ef4f0b130cdfd6769b7678df
Olatunji Peter <polalekan526@gmail.com>
2023-12-26 03:13:03 -0800

    Footer added

---
+++ src/component/Footer.jsx
@@ -0,0 +1,37 @@
+const Footer = () => {
+    return ( 
+        <>
+            <section className="bg-white bg-opacity-90 px-6   w-full font-roboto text-private text-[15px]">
+                <div className="grid md:grid-cols-9 py-20 grid-cols-1 gap-4 border-b-[1px] border-private bo">
+                    <div className="col-span-2 w-full border-b-[1px] border-private md: border-0">
+                        <h2 className="font-lora text-[150%] font-bold text-black "><i>AFRICANGROUP</i></h2>
+                        <h4>Redielksdkasd asojsd </h4>
+                    </div>
+                    <div className="col-span-4 md:col-span-1">
+                        <h2 className="mb-6">USEFUL LINKS</h2>
+
+                        <h4 className="my-2">Blog</h4>
+                        <h4 className="my-2">Contact</h4>
+                        <h4 className="my-2">Privacy Policy</h4>
+
+                    </div>
+                    <div className="col-span-2">
+                        <h2 className="mb-6">OFFICE</h2>
+                        <h4 className="my-2"><i className="fa fa-building mr-2"></i> Blog</h4>
+                        <h4 className="my-2"><i className="fa fa-phone mr-2"></i>+234 076 4255 373</h4>
+                        <h4 className="my-2"><i className="fa fa-envelope mr-2"></i>+234</h4>
+
+
+                    </div>
+
+                    <div className="col-span-4">
+                        <h2>CAREERS</h2>
+                    </div>                
+                    </div>
+                    <div>Jesus is Lord</div>
+            </section>
+        </>
+     );
+}
+ 
+export default Footer;
