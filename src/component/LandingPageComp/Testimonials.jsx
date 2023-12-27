e5e7f1503935bd83c51de093100537940788f49d
Olatunji Peter <polalekan526@gmail.com>
2023-12-27 04:20:28 -0800

    Testimonials added

--- src/component/LandingPageComp/Testimonials.jsx
+++ src/component/LandingPageComp/Testimonials.jsx
@@ -1,13 +1,31 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 const Testimonials = () => {
     return ( 
         <>
-            <section className="bg-white px-6 w-full h-[500px] font-roboto">
-                <div className="py-32">
+            <section className="bg-white px-6 w-full md:h-[600px] h-[800px] sm:h-[900px] font-roboto">
+                <div className="py-28">
                     
                 
                 <h2 className="font-bold py-2"><i className="fa fa-th text-private mr-2 font-bold"></i>TESTIMONIALS</h2>
                 <h1 className="text-4xl font-playfair text-private my-4">Our client reviews. </h1>
-                
+
+            <div className=" bg-gray px-6 py-4 flex justify-center items-center bg-opacity-20 mt-10 md:h-[250px] h-[500px] ">
+                <Carousel showThumbs={false} autoPlay={true} ax>
+                <div>
+                   
+                    <p className=" text-center  py-2 font-roboto text-black text-[19px] pb-10 break-words"><i>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia aliquam aspernatur quaerat ut maxime, porro quam ullam error perspiciatis quasi omnis sequi doloremque eveniet inventore obcaecati! Debitis possimus ut maiores fugiat autem doloribus facilis explicabo labore voluptatibus, quibusdam, provident pariatur.</i> </p>
+                </div>
+                <div>
+                    
+                    <p className=" text-center  py-2 font-roboto text-black text-[19px] pb-10 break-all"><i>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia aliquam aspernatur quaerat ut maxime, porro quam ullam error perspiciatis quasi omnis sequi doloremque eveniet inventore obcaecati! Debitis possimus ut maiores fugiat autem doloribus facilis explicabo labore voluptatibus, quibusdam, provident pariatur.</i> </p>
+                </div>
+                <div>
+                <p className=" text-center  py-2 font-roboto text-black text-[19px] pb-10 break-all"><i>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia aliquam aspernatur quaerat ut maxime, porro quam ullam error perspiciatis quasi omnis sequi doloremque eveniet inventore obcaecati! Debitis possimus ut maiores fugiat autem doloribus facilis explicabo labore voluptatibus, quibusdam, provident pariatur.</i> </p>
+                </div>
+            </Carousel>
+            </div>
                 </div>
             </section>
         </>
