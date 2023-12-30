c8709f5c4088f3662a5b73ee4f8eb7f82ea240c5
Olatunji Peter <polalekan526@gmail.com>
2023-12-30 07:03:49 -0800

    Loader updated

---
+++ src/component/LandingPageComp/Load.jsx
@@ -0,0 +1,16 @@
+const LoadingComponent = () => {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="loading-container">
+          <div className="loading-border"></div>
+          <img
+            src={img} // Replace with your image URL
+            alt="Loading"
+            className="loading-image"
+          />
+        </div>
+      </div>
+    );
+  }
+  
+  export default LoadingComponent;
