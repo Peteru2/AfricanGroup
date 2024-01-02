dade599e673bd06f573641c895535003cba1e9a0
Olatunji Peter <polalekan526@gmail.com>
2024-01-02 05:31:34 -0800

    Newletter done

--- src/component/NewsLetter.jsx
+++ src/component/NewsLetter.jsx
@@ -5,7 +5,7 @@ const NewsLetter = () => {
             <section className="footer-child  grid md:grid-cols-2 gap-4 grid-cols-1 bg-vintage  py-8 px-8  md:mx-16 mx-6 flex  text-white ">
                 <div className="">
                     <h2 className="text-[25px] font-playfair mb-6">Subscribe & get more information</h2>
-                    <h4 className="text-[16px] font-roboto break-words w-[500px]">Get the latest updates about the latest developments and exciting news on how we are shaping the future!</h4>
+                    <h4 className="text-[16px] font-roboto break-words md:w-[500px]">Get the latest updates about the latest developments and exciting news on how we are shaping the future!</h4>
                 </div>
                 <div className="md:ml-auto  flex items-center"> 
                 <div className="md:ml-auto md:w-auto w-full grid md:grid-cols-2 grid-cols-1 gap-2 ">
