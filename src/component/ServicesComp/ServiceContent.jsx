2b29b01a70e6e3f54d650909a0babb8be56f3a8c
Olatunji Peter <polalekan526@gmail.com>
2024-01-06 15:56:54 -0800

    Services upgraded

--- src/component/ServicesComp/ServiceContent.jsx
+++ src/component/ServicesComp/ServiceContent.jsx
@@ -5,9 +5,21 @@ const ServiceContent = () => {
         <>
         <section>
             <BackgroundImgWord />
-            <div className="xl:mx-6 md:mx-0 mx-6 mt-[60px] font-roboto">
-                    <div className="">
+            <div className="xl:mx-6 md:mx-0  mx-6 md:mt-[140px] mt-[30px]  font-roboto">
+                    <div className="flex justify-center">
+                        <div>
                         <h4 className="text-private text-center">OUR SERVICES </h4>
+                        <h4 className="text-3xl text-center font-bold my-3">What Service We Provide</h4>
+                        
+                            <h5 className="md:w-[600px] text-center flex justify-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro sit, architecto dolores quam sequi hic quidem ut. Dolores, quasi rerum! Quae aperiam quia maxime et doloremque distinctio. Dolorum, doloremque. Molestias.
+
+                            </h5>
+                           
+                            </div>
+
+                    </div>
+                    <div className=" grid  grid-cols-3">
+                            
                     </div>
             </div>
         </section>
