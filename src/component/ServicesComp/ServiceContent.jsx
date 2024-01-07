6595eacc883cd643ed1a92332d1931703eec30b7
Olatunji Peter <polalekan526@gmail.com>
2024-01-07 14:54:10 -0800

    New Feature added to services

--- src/component/ServicesComp/ServiceContent.jsx
+++ src/component/ServicesComp/ServiceContent.jsx
@@ -19,26 +19,27 @@ const ServiceContent = () => {
                             </div>
 
                     </div>
-                    <div className=" grid  xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-9">
-                        <div className="relative">                          
-                            <img src={img} alt="img"  /> 
-                            <div className="absolute bg-public p-5 text-white mx-7 group   transition ease-in-out  duration-300 hover:bg-vintage bottom-[-150px]">
-                                <div className="relative">
-                                    <div className="bg-vintage p-4 group-hover:bg-public absolute top-[-44px]">
-                                        <h3>dasdfas</h3>
-                                    </div>
-                                </div>
-                                <h2 className="my-3 mt-6">Land Survey</h2>
-                                    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat, quae minima error debitis esse velit odit ratione exercitationem hic! Laborum id ipsa provident suscipit optio et placeat reprehenderit numquam.</h3>
-                            </div>
-                        </div>
+                    <div className=" grid  xl:grid-cols-3 md:grid-cols-2 mb-[90px] grid-cols-1 gap-9">
+                       
  
 
                         <div className="relative">                          
                             <img src={img} alt="img"  /> 
                             <div className="absolute bg-public p-5 text-white mx-7 group   transition ease-in-out  duration-300 hover:bg-vintage bottom-[-150px]">
-                                <div className="relative">
+                                <div className="relative ">
                                     <div className="bg-vintage p-4 group-hover:bg-public absolute top-[-44px]">
+                                        <h3>dasdfas</h3>
+                                    </div>
+                                </div>
+                                <h2 className="my-3 mt-6 ">Land Survey</h2>
+                                    <h3 className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat, quae minima error debitis esse velit odit ratione exercitationem hic! Laborum id ipsa provident suscipit optio et placeat reprehenderit numquam.</h3>
+                            </div>
+                        </div>
+                        <div className="relative">                          
+                            <img src={img} alt="img"  /> 
+                            <div className="absolute bg-vintage p-5 text-white mx-7 group   transition ease-in-out  duration-300 hover:bg-public bottom-[-150px]">
+                                <div className="relative">
+                                    <div className="bg-public p-4 group-hover:bg-vintage absolute top-[-44px]">
                                         <h3>dasdfas</h3>
                                     </div>
                                 </div>
