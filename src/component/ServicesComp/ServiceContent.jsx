e073f9c634ddd3686a3d4d5829d9f7a07d6c86d6
Olatunji Peter <polalekan526@gmail.com>
2024-01-08 03:20:02 -0800

    Changes made to service area

--- src/component/ServicesComp/ServiceContent.jsx
+++ src/component/ServicesComp/ServiceContent.jsx
@@ -1,6 +1,6 @@
 import BackgroundImgWord from "./BackgroundImgAndWord";
 import img from "../../assets/images/FirstSectionImage1.jpg"
-
+import ServiceData from "./ServiceData";
 const ServiceContent = () => {
     return ( 
         <>
@@ -21,25 +21,35 @@ const ServiceContent = () => {
                     </div>
                     <div className=" grid  xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-9">
                        
- 
-
-                        <div className="relative  md:mb-0 mb-[150px]">                          
+                        {
+                            ServiceData.map((service, index)=>{
+                                return(
+                                    <div>
+                                        <div className="relative  md:mb-[] mb-[180px]">                          
                             <img src={img} alt="img"  /> 
-                            <div className="absolute bg-public p-5 text-white mx-7 group   transition ease-in-out  duration-300 hover:bg-vintage bottom-[-150px]">
+                            <div className={service.class1}>
                                 <div className="relative ">
-                                    <div className="bg-vintage p-4 group-hover:bg-public absolute top-[-44px]">
-                                        <h3>dasdfas</h3>
+                                    <div className={service.class2}>
+                                        <h3>J</h3>
                                     </div>
                                 </div>
-                                <h2 className="my-3 mt-6 ">Land Survey</h2>
-                                    <h3 className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat, quae minima error debitis esse velit odit ratione exercitationem hic! Laborum id ipsa provident suscipit optio et placeat reprehenderit numquam.</h3>
+                                <h2 className="my-3 mt-6 text-[19px] text-vintage group-hover:text-white">{service.title}</h2>
+                                    <h3 className={ ${service.classContent}}>{service.content}</h3>
+                                    <h4 className="my-4 text-vintage group-hover:text-white">Read More</h4>
                             </div>
                         </div>
-                        <div className="relative">                          
+                                    </div>
+                                )
+                            })
+                        }
+
+                        
+                        <div>
+                        <div className="relative mb-[150px] ">                          
                             <img src={img} alt="img"  /> 
-                            <div className="absolute bg-vintage p-5 text-white mx-7 group   transition ease-in-out  duration-300 hover:bg-public bottom-[-150px]">
+                            <div className="absolute bg-vintage p-5 text-white mx-7 group   transition ease-in-out  duration-300 hover:bg-private bottom-[-150px]">
                                 <div className="relative">
-                                    <div className="bg-public p-4 group-hover:bg-vintage absolute top-[-44px]">
+                                    <div className="bg-private p-4 group-hover:bg-vintage absolute top-[-44px]">
                                         <h3>dasdfas</h3>
                                     </div>
                                 </div>
@@ -47,12 +57,12 @@ const ServiceContent = () => {
                                     <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam placeat, quae minima error debitis esse velit odit ratione exercitationem hic! Laborum id ipsa provident suscipit optio et placeat reprehenderit numquam.</h3>
                             </div>
                         </div>
- 
-                        <div className="relative">                          
+                        </div>
+                        <div className="relative mb-[150px]">                          
                             <img src={img} alt="img"  /> 
-                            <div className="absolute bg-public p-5 text-white mx-7 group   transition ease-in-out  duration-300 hover:bg-vintage bottom-[-150px]">
+                            <div className="absolute bg-private p-5 text-white mx-7 group   transition ease-in-out  duration-300 hover:bg-vintage bottom-[-150px]">
                                 <div className="relative">
-                                    <div className="bg-vintage p-4 group-hover:bg-public absolute top-[-44px]">
+                                    <div className="bg-vintage p-4 group-hover:bg-private absolute top-[-44px]">
                                         <h3>dasdfas</h3>
                                     </div>
                                 </div>
---
+++ src/component/ServicesComp/ServiceData.js
@@ -0,0 +1,26 @@
+import firstImg from "../../assets/images/FirstSectionImage1.jpg"
+const ServiceData = [
+    {
+      id: 1,
+      title: " Land Surveying And Engineering Surveying",
+      content: "Cadastral Surveying, Land/Property Registration as a legal instrument, Property Detailing, Road/Route Survey, Topographical Survey (Determination of Spot levels/Height, Contours, 3D Wireframe, Direction of water flow), Hydrographical Survey and Instrument Trainers.",
+      img: firstImg,
+      class1: "absolute bg-private p-5 text-white mx-7 group   transition ease-in-out  duration-300 hover:bg-vintage bottom-[-250px]",
+      class2:"bg-vintage p-4 group-hover:bg-private absolute top-[-44px]",
+      classTitle:"text-vintage group-hover:text-white",
+      classContent:"text-white group-hover:text-opacity-70",
+      classRead:"text-vintage group-hover:text-white"
+    },
+    {
+        id: 2,
+        title: "Mapping & Geo - Data Solution",
+        content: "Map production with up-to date equipment’s, creation of visual representation of spatial data, analyze and manage geographic data including spatial relationship and patterns, Solving geographical problems with Real time satellite imagery, etc.",
+        img: firstImg,
+        class1: "absolute bg-vintage p-5 text-white mx-7 group   transition ease-in-out  duration-300 hover:bg-private bottom-[-250px]",
+        class2:"bg-private p-4 group-hover:bg-vintage absolute top-[-44px]"
+      },
+    
+   
+  ];
+  
+  export default ServiceData;
