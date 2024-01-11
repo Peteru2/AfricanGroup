5d5423c8644316b806a0ca27748ed81099257cc4
Olatunji Peter <polalekan526@gmail.com>
2024-01-11 04:03:37 -0800

    Form issue resolved

--- src/component/Navbar/QuoteForm.jsx
+++ src/component/Navbar/QuoteForm.jsx
@@ -1,17 +1,79 @@
+import { useState } from "react";
 const QuoteForm = ({close}) => {
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phoneNumber: '',
+      });
+    
+      const [errors, setErrors] = useState({
+        name: '', 
+        email: '',
+        phoneNumber: '',
+      });
+    
+      const handleInputChange = (e) => {
+        e.preventDefault();       
+        
+       const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+      };
+      const handleSubmit = (e) =>{
+            e.preventDefault();
+
+      const newErrors = {};
+   
+            if (formData.name.trim() === '') {
+
+            newErrors.name = 'Name is required';
+            console.log(errors.name)
+            
+        }
+
+            const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+                if (!formData.email.trim()) {
+                    newErrors.email = 'Email is required';
+                    console.log(errors.email)
+
+                } else if (!emailRegex.test(formData.email)) {
+                    newErrors.email = 'Invalid email format';
+                }
+
+            
+                const phoneRegex = /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+                if (!formData.phoneNumber.trim()) {
+                    newErrors.phoneNumber = 'Phone Number is required';
+                } else if (!phoneRegex.test(formData.phoneNumber)) {
+                    newErrors.phoneNumber = 'Invalid phone number format';
+                }
+
+
+            if (Object.keys(newErrors).length === 0) {
+                setErrors({});
+            }
+
+      }
     return ( 
                 <>
                     <section>
-                        <form className="font-roboto text-black">
+                        <form className="font-roboto text-black" onSubmit={handleSubmit}>
                             <div className="flex items-center ">
                             <h2 className="my-2 text-black font-playfair font-bold text-[20px]">Request for Quote</h2>
                             <button className="ml-auto cursor-pointer" onClick={(e) => { e.preventDefault(); close(); }}><i className="fa fa-times"></i></button>
                             </div>
                         <div className="mb-6">  
-                            <label className="my-5 label text">Name</label>
+                        
+                        <label className="my-5 label flex"><span>Name</span> <span className="ml-auto"> {errors.name}</span></label> 
+                      
                                 <div className="flex items-center border border-gray rounded-[5px] mt-1 px-3  py-2">
                                 <i className="fa fa-user text-gray w-5 h-5  mr-2 mt-1"></i>
-                                <input type="text" className="w-full outline-none text-black" placeholder="Name" />
+                                <input type="text" className="w-full outline-none text-black"
+                                autoFocus 
+                                name="name"
+                                value={formData.name}
+                                onChange={handleInputChange}
+                                placeholder="Name" />
                                 </div>
                             </div>
 
@@ -19,7 +81,11 @@ const QuoteForm = ({close}) => {
                             <label className="my-5 label">Email</label>
                                 <div className="flex items-center border border-gray rounded-[5px] px-3  mt-1 py-2">
                                 <i className="fa fa-envelope  w-5 h-5 text-gray  mr-2 mt-1"></i>
-                                <input type="Email" className="w-full outline-none text-black" placeholder="Email" />
+                                <input type="Email" 
+                                name="email"
+                                value={formData.email} 
+                                onChange={handleInputChange}
+                                className="w-full outline-none text-black" placeholder="Email" />
                                 </div>
                             </div>
 
@@ -27,7 +93,11 @@ const QuoteForm = ({close}) => {
                             <label className="my-5 label">Phone Number</label>
                                 <div className="flex items-center border border-gray rounded-[5px] px-3 mt-1  py-2">
                                 <i className="fa fa-phone text-gray w-5 h-5  mr-2 mt-1"></i>
-                                <input type="text" className="w-full outline-none text-black" placeholder="Phone Number" />
+                                <input type="text" 
+                                name="phoneNumber"
+                                value={formData.phoneNumber}
+                                onChange={handleInputChange}
+                                className="w-full outline-none text-black" placeholder="Phone Number" />
                                 </div>
                             </div>
 
@@ -39,7 +109,7 @@ const QuoteForm = ({close}) => {
                                 </div>
                             </div>
 
-                            <button className="bg-private hover:bg-private hover:bg-opacity-90  text-white font-semibold py-4 px-4 rounded w-full">Submit</button>
+                            <button  type="submit" className="bg-private hover:bg-private hover:bg-opacity-90  text-white font-semibold py-4 px-4 rounded w-full">Submit</button>
                                 
                         </form>
                     </section>
