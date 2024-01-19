a672b59ad17d5fa3faf59eca339281b74407130b
Olatunji Peter <polalekan526@gmail.com>
2024-01-19 07:04:44 -0800

    Blog Post page modified

--- src/component/BlogComp/data.js
+++ src/component/BlogComp/data.js
@@ -2,7 +2,7 @@ import firstImg from "../../assets/images/FirstSectionImage1.jpg"
 const blogData = [
     {
       id: 1,
-      titleParam:"Blog_Post_1",
+      titleParam:"blog-post-1",
       title: "Blog Post 1",
       date: "January 1, 2023",
       content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...",
@@ -12,6 +12,7 @@ const blogData = [
     },
     {
         id: 2,
+      titleParam:"blog-post-2",
         title: "Blog Post 2",
         date: "February 6, 2003",
         content: "Oh Lord our in ages past",
