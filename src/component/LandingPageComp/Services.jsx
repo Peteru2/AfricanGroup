6dfe5e6effbec9d82f10e84d73347b52bd95a035
Olatunji Peter <polalekan526@gmail.com>
2024-02-01 00:12:38 +0100

    Learn more button added

--- src/component/LandingPageComp/Services.jsx
+++ src/component/LandingPageComp/Services.jsx
@@ -67,7 +67,15 @@ const firstThreeServiceItems = ServiceData.slice(0,3);
                                     })
                                 }
                                
-                                
+                               
+                                  <Link to={"/Services"}>
+                                  <div className="flex w-full">
+                                  <div className="ml-auto text-center border-2 rounded-full h-[30px] transition ease-in-out delay-150 duration-900 w-[120px] border-vintage bg-vintage hover:bg-white hover:text-vintage text-white">
+                                 <button>Learn More</button>
+                                  </div>
+                                </div>
+
+                                  </Link>
                                 
                                  </Bounce>
                                 
