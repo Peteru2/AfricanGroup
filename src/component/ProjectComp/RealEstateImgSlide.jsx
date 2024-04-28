8713796bed9e57a4465b3ad6b9f87068ae2a15c1
Olatunji Peter <polalekan526@gmail.com>
2024-04-28 16:35:48 +0100

    New Update

--- src/component/ProjectComp/RealEstateImgSlide.jsx
+++ src/component/ProjectComp/RealEstateImgSlide.jsx
@@ -1,9 +1,62 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import img from "../../assets/images/FirstSectionImage1.jpg"
 
 const RealEstateImgSlide = () => {
     const sliderRef = useRef(null);
-     const scrollAmount = 1200; 
+    const [showButtons, setShowButtons] = useState(true);
+    const scrollAmountDesktop = 840; 
+    const scrollAmountMobile = 400; 
+    console.log(window.innerWidth)
+
+    const scrollAmount = window.innerWidth <= 768 ? scrollAmountMobile : scrollAmountDesktop;
+    useEffect(() => {
+        const container = sliderRef.current;
+        let startX = 0;
+        let isScrolling = false;
+
+        const handleTouchStart = (e) => {
+            isScrolling = true;
+            startX = e.touches[0].clientX;
+        };
+
+        const handleTouchMove = (e) => {
+            if (!isScrolling) return;
+            const distance = startX - e.touches[0].clientX;
+            container.scrollLeft += distance;
+            startX = e.touches[0].clientX;
+        };
+
+        const handleTouchEnd = () => {
+            isScrolling = false;
+        };
+
+        container.addEventListener("touchstart", handleTouchStart);
+        container.addEventListener("touchmove", handleTouchMove);
+        container.addEventListener("touchend", handleTouchEnd);
+
+        return () => {
+            container.removeEventListener("touchstart", handleTouchStart);
+            container.removeEventListener("touchmove", handleTouchMove);
+            container.removeEventListener("touchend", handleTouchEnd);
+        };
+    }, []);
+
+    // Toggle visibility of navigation buttons based on screen width
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 768) {
+                setShowButtons(false);
+            } else {
+                setShowButtons(true);
+            }
+        };
+
+        window.addEventListener("resize", handleResize);
+        handleResize();
+
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     return ( 
         <>
         <div className="slide">
