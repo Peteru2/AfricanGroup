ee943c40c3e829604aca7416a3a01d357e610e46
Olatunji Peter <polalekan526@gmail.com>
2024-05-09 00:15:54 +0100

    New Update

--- src/component/LandingPageComp/Partners.jsx
+++ src/component/LandingPageComp/Partners.jsx
@@ -95,10 +95,67 @@ const Partners = () => {
                 <img src={raybashInt} alt="lll"/>
             </div>
             
+       
+            
+        <div class="logo">
+                <img src={realTime} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={brainHammers} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={rcc} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={goFarms} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={oreOfePlastics} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={oyoStateGov} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={fairGreens} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={afriProd} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={raybashInt} alt="lll"/>
+            </div>
         </div>
         <div class="track">
             
         <div class="logo">
+                <img src={realTime} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={brainHammers} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={rcc} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={goFarms} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={oreOfePlastics} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={oyoStateGov} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={fairGreens} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={afriProd} alt="lll"/>
+            </div>
+            <div class="logo">
+                <img src={raybashInt} alt="lll"/>
+            </div>
+
+            <div class="logo">
                 <img src={realTime} alt="lll"/>
             </div>
             <div class="logo">
